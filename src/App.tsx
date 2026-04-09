2680941d539d1d3428cf4abf865cf6df4bbdb05b
Owensom <somervilleowen@gmail.com>
2026-04-09 21:03:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PublicRafflePage from "./pages/PublicRafflePage";
 
 function HomePage() {
   return (
     <div style={{ padding: 24 }}>
       <h1>Raffle Platform</h1>
-      <Link to="/r/demo-raffle">
-        <button type="button">Open Demo Raffle</button>
-      </Link>
+
+      <div style={{ marginTop: 16 }}>
+        <a href="/r/demo-raffle">
+          <button type="button">Open Demo Raffle</button>
+        </a>
+      </div>
     </div>
   );
-}
-
-function SafeAdminPlaceholder() {
-  return <div style={{ padding: 24 }}>Admin placeholder</div>;
 }
 
 export default function App() {
@@ -21,7 +20,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<SafeAdminPlaceholder />} />
         <Route path="/r/:slug" element={<PublicRafflePage />} />
       </Routes>
     </BrowserRouter>
