fc2e5ffbf948bdaf0b9bf73680485931519befd1
Owensom <somervilleowen@gmail.com>
2026-04-09 13:30:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
+import ProtectedRoute from "./components/ProtectedRoute";
 import AdminPage from "./pages/AdminPage";
-
-// You can replace these with real components later
-function Home() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Raffle App</h1>
-      <p>Welcome to the platform.</p>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Login</h1>
-      <p>TODO: Build login form</p>
-    </div>
-  );
-}
-
-function Register() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Register</h1>
-      <p>TODO: Build register form</p>
-    </div>
-  );
-}
-
-function PublicRaffle() {
-  const [raffle, setRaffle] = useState<any>(null);
-
-  const slug = window.location.pathname.split("/").pop();
-
-  useEffect(() => {
-    fetch(`/api/public/raffles/${slug}`)
-      .then((res) => res.json())
-      .then((data) => setRaffle(data));
-  }, [slug]);
-
-  if (!raffle) return <div>Loading raffle...</div>;
-
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>{raffle.title}</h1>
-      <p>{raffle.description}</p>
-    </div>
-  );
-}
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import PublicRafflePage from "./pages/PublicRafflePage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Core pages */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-        {/* Admin */}
-        <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminPage />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Public buyer page */}
-        <Route path="/r/:slug" element={<PublicRaffle />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="/r/:slug" element={<PublicRafflePage />} />
+    </Routes>
   );
 }
