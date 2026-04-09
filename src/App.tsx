fe30f6ca4acffc57fa433a918061a37d3b7eb60b
Owensom <somervilleowen@gmail.com>
2026-04-09 20:51:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import PublicRafflePage from "./pages/PublicRafflePage";
+
+function HomePage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Raffle Platform</h1>
+      <p>Home page is working.</p>
+
+      <div style={{ display: "flex", gap: 12, flexWrap: "wrap" }}>
+        <Link to="/r/demo-raffle">
+          <button type="button">Open Demo Raffle</button>
+        </Link>
+
+        <Link to="/admin">
+          <button type="button">Open Admin</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminPage />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/r/:slug" element={<PublicRafflePage />} />
       </Routes>
     </BrowserRouter>
