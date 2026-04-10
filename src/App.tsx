103aaaa31ee414091ba8535289658de4da7d9229
Owensom <somervilleowen@gmail.com>
2026-04-10 13:50:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PublicRafflePage from "./PublicRafflePage";
 
 function HomePage() {
   return (
     <div style={{ padding: 24 }}>
       <h1>HOME TEST</h1>
       <a href="/r/demo-raffle">Go to raffle test</a>
-    </div>
-  );
-}
-
-function PublicRafflePage() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>PUBLIC RAFFLE TEST</h1>
-      <p>If you can see this, the route is working.</p>
     </div>
   );
 }
