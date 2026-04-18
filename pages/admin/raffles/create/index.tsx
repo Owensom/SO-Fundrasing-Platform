19ddba408018b61dda5250dfc73f9098e910f861
Owensom <somervilleowen@gmail.com>
2026-04-18 11:00:55 +0100

    Update index.tsx

--- pages/admin/raffles/create/index.tsx
+++ pages/admin/raffles/create/index.tsx
@@ -1,1 +1,14 @@
-export { default } from "../../../../src/pages/admin/AdminCreateRafflePage";
+import type { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/admin/raffles",
+      permanent: false,
+    },
+  };
+};
+
+export default function LegacyAdminCreateRaffleRedirect() {
+  return null;
+}
