cf10bf3afdaaf3e5e66368bb8d52c790ec56397d
Owensom <somervilleowen@gmail.com>
2026-04-18 11:19:53 +0100

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,6 +2,14 @@ import "next-auth";
 import "next-auth/jwt";
 
 declare module "next-auth" {
+  interface User {
+    id: string;
+    email: string;
+    name?: string | null;
+    tenantSlugs: string[];
+    emailVerified?: Date | null;
+  }
+
   interface Session {
     user: {
       id: string;
@@ -9,13 +17,6 @@ declare module "next-auth" {
       name?: string | null;
       tenantSlugs: string[];
     };
-  }
-
-  interface User {
-    id: string;
-    email: string;
-    name?: string | null;
-    tenantSlugs: string[];
   }
 }
 
