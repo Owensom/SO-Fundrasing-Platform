073ee7cfee8489afd56f30fe185ba612d4169a1e
Owensom <somervilleowen@gmail.com>
2026-04-18 14:47:36 +0100

    Delete app/admin/raffles/new directory

--- app/admin/raffles/new/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function NewRafflePage() {
-  return (
-    <main style={{ maxWidth: 960, margin: "40px auto", padding: "0 16px" }}>
-      <h1>Create raffle</h1>
-      <p>App Router admin create page is working.</p>
-    </main>
-  );
-}
