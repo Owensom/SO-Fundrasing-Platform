c584fdd743a4fe8da8b4b20896240f7f8c38cc89
Owensom <somervilleowen@gmail.com>
2026-04-18 14:37:09 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "SO Fundraising Platform",
+  description: "Multi-tenant fundraising platform",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
