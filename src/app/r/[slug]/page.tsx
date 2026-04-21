965a7e1cdd402c4bdc694f9d40decf16a56ac42c
Owensom <somervilleowen@gmail.com>
2026-04-21 20:07:24 +0100

    Update page.tsx

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,5 +1,4 @@
-import type { ComponentType } from "react";
-import * as PublicRaffleModule from "@/components/PublicRafflePage";
+import PublicRafflePage from "@/components/PublicRafflePage";
 
 type PageProps = {
   params: {
@@ -7,15 +6,6 @@ type PageProps = {
   };
 };
 
-const PublicRafflePage = (
-  (PublicRaffleModule as any).default ??
-  (PublicRaffleModule as any).PublicRafflePage
-) as ComponentType<{ slug: string }>;
-
 export default function RafflePage({ params }: PageProps) {
-  if (!PublicRafflePage) {
-    return <div>Public raffle page component not found.</div>;
-  }
-
   return <PublicRafflePage slug={params.slug} />;
 }
