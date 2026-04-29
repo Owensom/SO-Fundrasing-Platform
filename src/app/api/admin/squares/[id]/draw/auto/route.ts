03106dd439875bce71f68de6c3da53b2b3ba3202
Owensom <somervilleowen@gmail.com>
2026-04-29 16:58:47 +0100

    Create route.ts

---
+++ src/app/api/admin/squares/[id]/draw/auto/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import {
+  createSquaresWinner,
+  getSquaresGameById,
+  listSquaresSales,
+  listSquaresWinners,
+  updateSquaresGame,
+} from "../../../../../../../../api/_lib/squares-repo";
