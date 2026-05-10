f281f7f88369a8c4073eae505620f329d2c0ef8a
Owensom <somervilleowen@gmail.com>
2026-05-10 18:11:34 +0100

    Create page.tsx

---
+++ src/app/a/page.tsx
@@ -0,0 +1,8 @@
+export default function AuctionRouteTestPage() {
+  return (
+    <main style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>Auction route test</h1>
+      <p>If you can see this, the /a route exists.</p>
+    </main>
+  );
+}
