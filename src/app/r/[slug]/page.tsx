b03f190b95a933b8a3607164434bbe1471defaeb
Owensom <somervilleowen@gmail.com>
2026-05-22 15:28:37 +0100

    Update page.tsx

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import PublicRafflePage from "@/components/PublicRafflePage";
+import * as PublicRafflePageModule from "@/components/PublicRafflePage";
 
 export const dynamic = "force-dynamic";
 
@@ -8,8 +8,30 @@ type PageProps = {
   }>;
 };
 
+type PublicRafflePageProps = {
+  slug: string;
+};
+
+const PublicRafflePage = (
+  PublicRafflePageModule as unknown as {
+    default?: (props: PublicRafflePageProps) => JSX.Element;
+    PublicRafflePage?: (props: PublicRafflePageProps) => JSX.Element;
+  }
+).default ?? (
+  PublicRafflePageModule as unknown as {
+    default?: (props: PublicRafflePageProps) => JSX.Element;
+    PublicRafflePage?: (props: PublicRafflePageProps) => JSX.Element;
+  }
+).PublicRafflePage;
+
 export default async function RaffleSlugPage({ params }: PageProps) {
   const { slug } = await params;
+
+  if (!PublicRafflePage) {
+    throw new Error(
+      "PublicRafflePage component was not exported from src/components/PublicRafflePage.tsx.",
+    );
+  }
 
   return <PublicRafflePage slug={slug} />;
 }
